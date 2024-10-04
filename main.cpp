8a881f55be36a8133033a8921595a83ee0b72026
Agustín Ruiz <aeruiz@fi.uba.ar>
2024-10-04 00:19:49 +0000

    Adding code changes

--- main.cpp
+++ main.cpp
@@ -1,23 +1,37 @@
 #include "mbed.h"
 #include "arm_book_lib.h"
+#include "hal/gpio_api.h"
 
 int main()
 {
-    DigitalIn gasDetector(D2);
 
-    DigitalOut alarmLed(LED1);
+    gpio_t gas_detector;
+    gpio_t alarm_led;
 
-    gasDetector.mode(PullDown);
+    gpio_init_in(&gas_detector, D2);
+    //DigitalIn gasDetector(D2);
 
-    alarmLed = OFF;
+    gpio_init_out(&alarm_led, LED1);
+    
+    //DigitalOut alarmLed(LED1);
+
+    gpio_mode(&gas_detector, PullDown);
+    //gasDetector.mode(PullDown);
+
+    //alarmLed = OFF;
+    gpio_write(&alarm_led, OFF);
 
     while (true) {
-        if ( gasDetector == ON ) {
-            alarmLed = ON;
+
+        
+        if ( gpio_read(&gas_detector) == ON ) {
+            gpio_write(&alarm_led, ON);
+            //alarmLed = ON;
         }
         
-        if ( gasDetector == OFF ) {
-            alarmLed = OFF;
+        if ( gpio_read(&gas_detector) == OFF ) {
+            gpio_write(&alarm_led, OFF);
+            //alarmLed = OFF;
         }
     }
 }
